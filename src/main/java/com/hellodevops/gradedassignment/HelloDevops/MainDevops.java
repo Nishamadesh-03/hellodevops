e4adc4df67400949b68f19edb983d50e14f2de99
Nishamadesh03 <nisham@hcl.com>
2021-12-12 19:46:05 +0530

    first commit to springboot

---
+++ src/main/java/com/hellodevops/gradedassignment/HelloDevops/MainDevops.java
@@ -0,0 +1,13 @@
+package com.hellodevops.gradedassignment.HelloDevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainDevops {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainDevops.class, args);
+		System.out.println("Hello Dev-Ops");	}
+
+}
---
+++ src/test/java/com/hellodevops/gradedassignment/HelloDevops/MainDevopsTests.java
@@ -0,0 +1,13 @@
+package com.hellodevops.gradedassignment.HelloDevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainDevopsTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
